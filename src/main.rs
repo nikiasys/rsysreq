88e7b78c85d10932946feed1e7264ca421e39d5c
Luben <114294912+67x18@users.noreply.github.com>
2023-06-22 23:39:49 +0000

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,11 @@ use std::fs::File;
 use std::io::{BufRead, BufReader};
 
 fn main() {
+    if let Ok(username) = env::var("USER") {
+        println!("Username: {}", username);
+    } else {
+        println!("Username: not found.");
+    }
     if let Ok(os_release) = fs::read_to_string("/etc/os-release") {
         let mut distro_name = None;
         let mut distro_version = None;
@@ -18,7 +23,13 @@ fn main() {
                 break;
             }
         }
+        let output = Command::new("uname")
+        .arg("-r")
+        .output()
+        .expect("Failed to run command.");
 
+        let kernel_version = String::from_utf8_lossy(&output.stdout);
+        println!("Kernel Version: {}", kernel_version.trim());
         if let Some(name) = distro_name {
             println!("Linux Distribution: {}", name);
         }
@@ -28,6 +39,14 @@ fn main() {
     } else {
         println!("Linux distribution information not found.");
     }
+    let output = Command::new("uname")
+        .arg("-r")
+        .output()
+        .expect("Failed to run command.");
+
+    let kernel_version = String::from_utf8_lossy(&output.stdout);
+    println!("Kernel Version: {}", kernel_version.trim());
+
     if let Ok(file) = File::open("/proc/cpuinfo") {
         let reader = BufReader::new(file);
 
@@ -52,26 +71,38 @@ fn main() {
     let mem_total = free.lines().skip(1).next().unwrap().split_whitespace().nth(1).unwrap();
     println!("Total Memory: {}", mem_total);
 
-    let output = Command::new("df").arg("-h").output().expect("Failed to Run Command.");
-    let df = String::from_utf8_lossy(&output.stdout);
-    let total_space = df.lines().skip(1).next().unwrap().split_whitespace().nth(1).unwrap();
-    println!("Total Disk Space: {}", total_space);
+    let output = Command::new("df")
+        .arg("-h")
+        .arg("/")
+        .output()
+        .expect("Failed to run command.");
 
-    let output = Command::new("lspci").arg("-v").output().expect("Failed to Run Command.");
-    let lspci = String::from_utf8_lossy(&output.stdout);
-    let mut driver_found = false;
-    for line in lspci.lines() {
-        if line.starts_with("VGA compatible controller:") {
-            let parts: Vec<&str> = line.split(": ").collect();
-            if parts.len() > 1 {
-                println!("GPU Driver: {}", parts[1]);
-                driver_found = true;
-                break;
-            }
-        }
-    }
-    if !driver_found {
-        println!("GPU Driver: not found.");
+    let df = String::from_utf8_lossy(&output.stdout);
+    let disk_space_line = df.lines().skip(1).next().unwrap();
+    let disk_space_parts: Vec<&str> = disk_space_line.split_whitespace().collect();
+    let disk_usage = disk_space_parts[2];
+    let total_space = disk_space_parts[1];
+    let available_space = disk_space_parts[3];
+
+    println!("Disk Usage: {}", disk_usage);
+    println!("Total Disk Space: {}", total_space);
+    println!("Available Disk Space: {}", available_space);
+
+    let output = Command::new("sh")
+        .arg("-c")
+        .arg("lsmod | grep -o '^[^ ]*' | grep -E '^(amdgpu|nvidia|nouveau|radeon)'")
+        .output()
+        .expect("GPU Driver: Failed to run command.");
+
+    let stdout = String::from_utf8_lossy(&output.stdout);
+    let stderr = String::from_utf8_lossy(&output.stderr);
+
+    if !stdout.is_empty() {
+        let drivers: Vec<&str> = stdout.trim().split('\n').collect();
+        let drivers_combined = drivers.join(", ");
+        println!("GPU Drivers: {}", drivers_combined);
+    } else {
+        println!("GPU Driver: not found. {}", stderr);
     }
     let output = Command::new("lspci").arg("-v").output().expect("Failed to Run Command.");
     let lspci = String::from_utf8_lossy(&output.stdout);
@@ -92,5 +123,5 @@ fn main() {
         println!("Display Server: Wayland ({})", wayland_display);
     } else {
         println!("Display Server: Xorg");
-    }
+    };
 }
